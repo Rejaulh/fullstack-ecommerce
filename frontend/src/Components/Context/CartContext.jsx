7eeb1ade706c9a8ee12f57243fc037684d349c3b
Rejaulh <rejaulassam@gmail.com>
2026-03-17 13:24:23 +0530

    update context file

--- frontend/src/Components/Context/CartContext.jsx
+++ frontend/src/Components/Context/CartContext.jsx
@@ -1,48 +1,71 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
 
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
+  const BASEURL = import.meta.env.VITE_DJANGO_BASE_URL;
   const [cartItems, setCartItems] = useState([]);
+  const [total, setTotal] = useState(0);
 
-  // Add product to cart
-  const addToCart = (product) => {
-    const exist = cartItems.find((item) => item.id === product.id);
-    if (exist) {
+
+  const fetchCart = async () => {
+    try {
+      const response = await fetch(`${BASEURL}/api/cart/`)
+      if(!response.ok){
+        throw new Error("Failed to fetch cart");
+      }
+      const data = await response.json();
+      setCartItems(data.item || []);
+      setTotal(data.total || 0);
+
+    } catch (error) {
+      console.error("Error fatching cart:", error);
+    }
+  }
+
+  useEffect(()=>{
+    fetchCart();
+  }, []);
+
+  
+    // Add product to cart
+    const addToCart = (product) => {
+      const exist = cartItems.find((item) => item.id === product.id);
+      if (exist) {
+        setCartItems(
+          cartItems.map((item) =>
+            item.id === product.id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          )
+        );
+      } else {
+        setCartItems([...cartItems, { ...product, quantity: 1 }]);
+      }
+    };
+
+
+    // Remove product from cart
+    const removeFromCart = (id) => {
+      setCartItems(cartItems.filter(item => item.id !== id));
+    };
+
+    // Update Quantity
+    const updateQuantity = (id, quantity) => {
+      if (quantity < 1) return;
       setCartItems(
         cartItems.map((item) =>
-          item.id === product.id
-            ? { ...item, quantity: item.quantity + 1 }
-            : item
+          item.id === id ? { ...item, quantity } : item
         )
       );
-    } else {
-      setCartItems([...cartItems, { ...product, quantity: 1 }]);
-    }
+    };
+
+    return (
+      <CartContext.Provider value={{ cartItems, addToCart, removeFromCart, updateQuantity }}>
+        {children}
+      </CartContext.Provider>
+    );
   };
-
-
-// Remove product from cart
-const removeFromCart = (id) => {
-  setCartItems(cartItems.filter(item => item.id !== id));
-};
-
-// Update Quantity
-const updateQuantity = (id, quantity) => {
-  if (quantity < 1) return;
-  setCartItems(
-    cartItems.map((item) =>
-      item.id === id ? { ...item, quantity } : item
-    )
-  );
-};
-
-return (
-  <CartContext.Provider value={{ cartItems, addToCart, removeFromCart, updateQuantity }}>
-    {children}
-  </CartContext.Provider>
-  );
-};
 
 // export const useCart = () => useContext(CartContext);
